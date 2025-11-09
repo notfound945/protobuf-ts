c5e9691c0d7a89ee00fc56e478a05b5ad81e3460
Purple Hydrogen Leak <1617291164@qq.com>
2025-11-09 16:00:43 +0800

    feat: overwrite

--- packages/test-export_client/spec/export-client-options.spec.ts
+++ packages/test-export_client/spec/export-client-options.spec.ts
@@ -1,10 +1,34 @@
-import {TestService} from '../gen/exportclient';
-describe('spec.MethodExportClient', function () {
-    it('should have option "exportclient"', function () {
-       console.debug(TestService.methods[1].options);
-       expect(TestService.methods[1].options).toBeDefined();
-       if (TestService.methods[1].options) {
-        expect(TestService.methods[1].options["blocker.exportclient"]).toBe(0);
-       }
-    });
-})
+import { ServiceWithExportClient } from "../gen/exportclient";
+
+describe("spec.ServiceWithExportClient.Method", function () {
+  it('should have option "exportclient"', function () {
+    console.debug(ServiceWithExportClient.methods[0].options);
+    expect(ServiceWithExportClient.methods[0].options).toBeDefined();
+  });
+});
+describe("spec.ServiceWithExportClient.MethodWithExportClient0", function () {
+  it('should have option "exportclient"', function () {
+    console.debug(ServiceWithExportClient.methods[1].options);
+    expect(ServiceWithExportClient.methods[1].options).toBeDefined();
+    if (ServiceWithExportClient.methods[1].options) {
+      expect(
+        ServiceWithExportClient.methods[1].options["blocker.exportclient"]
+      ).toBe(0);
+    }
+  });
+});
+
+describe("spec.ServiceWithExportClient.Method1", function () {
+  it('should not have option "exportclient"', function () {
+    console.debug(ServiceWithExportClient.methods[2].options);
+    expect(ServiceWithExportClient.methods[2].options).toBeDefined();
+    if (ServiceWithExportClient.methods[2].options) {
+      expect(
+        ServiceWithExportClient.methods[2].options["blocker.exportclient"]
+      ).toBeUndefined();
+      expect(
+        ServiceWithExportClient.methods[2].options["blocker.exportclient"]
+      ).toBe(1);
+    }
+  });
+});
